db9eeeb6822831850e558ac7e422dc1d5e6b2d08
shreyoshipaul6-svg <shreyoshipaul6@gmail.com>
2026-01-09 21:09:42 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+first_number=int(input("Enter the first number: "))
+second_number=int(input("Enter the second number: "))
+result_of_addition=first_number+second_number
+print("result of addition is",result_of_addition)
+result_of_subtraction=first_number-second_number
+print("result of subtraction is",result_of_subtraction)
+result_of_multiplication=first_number*second_number
+print("result of multiplication is",result_of_multiplication)
+result_of_division=first_number/second_number
+print("result of division is",result_of_division)
---
+++ tast2.py
@@ -0,0 +1,6 @@
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print("Your first name is",first_name)
+print("Your last name is",last_name)
+full_name=first_name+" "+last_name
+print("Hello, " +full_name+ "!Welcome to python programming.")
